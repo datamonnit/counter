8ea9ce70c79fec05231f5353e12bcbd22d68c3c6
Tuomas <tuomas.puro@esedu.fi>
2019-12-11 09:59:32 +0200

    bootstrapin navbar lisätty

--- login.php
+++ login.php
@@ -17,6 +17,25 @@ if (isset($_SESSION['logged_in'])) {
 </head>
 <body>
 
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">CounterApp</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./">Counter</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+
+
     <div class="jumbotron">
     <h1>Login to counterApp</h1>
     <p>Follow all your important dates...</p>
@@ -35,5 +54,8 @@ if (isset($_SESSION['logged_in'])) {
             <input class="btn btn-primary" type="submit" value="Login">
         </form>
     </div>   
+
+    <script src="./js/jquery.min.js"></script>
+    <script src="./js/boostrap.min.js"></script>
 </body>
 </html>
