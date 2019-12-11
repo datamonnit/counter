7744b10640db2a82c306e3d14e78f9d54337fbbe
Tuomas <tuomas.puro@esedu.fi>
2019-12-11 10:11:06 +0200

    turha home-linkki pois

--- login.php
+++ login.php
@@ -34,9 +34,6 @@ if (isset($_SESSION['logged_in'])) {
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
                 <li class="nav-item">
                     <a class="nav-link" href="./">Counter</a>
                 </li>
